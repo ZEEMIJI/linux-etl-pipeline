5cd52857841797c3119ba4a6e54492a4a6130bd5
olaoye zainab <olaoyezainab@olaoyes-MacBook-Air.local>
2026-09-08 09:16:55 +0100

    added echo message

--- scripts/move_files.sh
+++ scripts/move_files.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
 mkdir -p json_and_CSV
-find ./source_files \( -name "*.csv" -o -name "*.json" \) -exec mv {} json_and_CSV/ \;
+
+if find ./source_files \( -name "*.csv" -o -name "*.json" \) | grep -q .; then
+    find ./source_files \( -name "*.csv" -o -name "*.json" \) -exec mv {} json_and_CSV/ \;
+    echo "CSV and/or JSON files moved successfully."
+else
+    echo "No CSV or JSON files in source_files."
+fi
